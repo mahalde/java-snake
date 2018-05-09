3dd66aa290e3c813b5defe3246ccb58055ebb6a1
Malte Thienel <malte.thienel@gmail.com>
2018-05-09 09:55:09 +0200

    Added static symbol variable

--- src/main/Wall.java
+++ src/main/Wall.java
@@ -2,8 +2,18 @@ package main;
 
 public class Wall extends GameObject {
 
+	private static char symbol;
+	
 	public Wall() {
 		super();
+	}
+
+	public static char getSymbol() {
+		return symbol;
+	}
+
+	public static void setSymbol(char symbol) {
+		Wall.symbol = symbol;
 	}
 
 	
