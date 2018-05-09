aa8238d428c734550b99f8a0afd8195fa7e51ef5
MIWEBER <MIWEBER@OLDN91992.btcnet.btc-ag.com>
2018-05-09 10:06:47 +0200

    Settings

--- src/startScreen/Settings.java
+++ src/startScreen/Settings.java
@@ -1,9 +1,35 @@
 package startScreen;
 
+import main.Snake;
+import sz.IO;
+
 public class Settings {
 
+	int height;
+	int width;
 	public Settings() {
-		// JUST SEEING IF WORKS
+		show();
+		aendern();
 	}
 
+	public void show() {
+		TitleScreen.showSnake();
+		System.out.println("      Einstellungen");
+		System.out.println();
+		System.out.println("1. Höhe(Symbole): " + this.height);
+		System.out.println("2. Breite(Symbole): " + this.width);
+		System.out.println("3. Schlangen Symbol: " + Snake.getSymbol());
+		System.out.println("4. Wand Symbol: " + Wall.getSymbol());
+		System.out.println("5. Frucht Symbol: " + Fruit.getSymbol());
+		System.out.println("6. Verlassen");
+	}
+	
+	public void aendern() {
+		int change;
+		do {
+			change = IO.readInt("Was möchten Sie ändern: ");
+		}while(change < 1 || change > 6);
+		
+		
+	}
 }
