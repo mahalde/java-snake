0fb697631b5cdab7665d7de88adc28e309d68138
MIWEBER <MIWEBER@OLDN91992.btcnet.btc-ag.com>
2018-05-24 15:50:34 +0200

    problem mit arraylist, überall 0

--- src/main/Wall.java
+++ src/main/Wall.java
@@ -14,6 +14,7 @@ public class Wall extends GameObject{
 		for(int zaehler = 0; zaehler < height; zaehler++) {
 			for(int zaehler2 = 0; zaehler2 < width; zaehler2++) {
 				if(x == 1 || x == height || y == 1 || y == width) {
+					int[] coordinate = new int[2];
 					this.coordinate[0] = zaehler;
 					this.coordinate[1] = zaehler2;
 					position.add(coordinate);
@@ -25,7 +26,12 @@ public class Wall extends GameObject{
 			y++;
 			x = 1;
 		}	
-			System.out.println(position.get(1)[1]);	
+			for(int i = 1; i < position.size(); i++) {
+				for(int j = 0; j < 2; j++) {
+					System.out.println(position.get(i)[j]);		
+				}
+			}
+
 	}
 	
 	public static char getSymbol() {
