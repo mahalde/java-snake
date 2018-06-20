ec646a41ffe98f7446369f38a319d9c6449ef8b9
MIWEBER <MIWEBER@OLDN91992.btcnet.btc-ag.com>
2018-06-20 08:39:16 +0200

    main

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,10 +9,7 @@ import screens.TitleScreen;
 public class Main {
 
 	public static void main(String[] args) {
-		TitleScreen screen = new TitleScreen(); /*
-												 * Erstellt den TitleScreen, dabei folgt die weitere Eingabe der z.B.
-												 * Einstellung über den User
-												 */
+		TitleScreen screen = new TitleScreen(); /* Erstellt den TitleScreen, dabei folgt die weitere Eingabe der z.B. Einstellung über den User */
 		do {
 			screen.show();
 		} while (screen.getSelected() == 2 || screen.getSelected() == 4);
